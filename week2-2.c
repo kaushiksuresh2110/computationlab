e47fac20b52700f13566f14e522fd190821db16f
kaushiksuresh2110 <31548662+kaushiksuresh2110@users.noreply.github.com>
2018-01-31 09:54:44 +0530

    Update week2-2.c

--- week2-2.c
+++ week2-2.c
@@ -1,14 +1,15 @@
+//to convert binary number to decimal 
 #include<stdio.h>
 int power(int , int );
 int length(int);
-int power(int z,int x)
+int power(int z,int x)   // to calculate the power of a number
 {int product=1;
 for(int i=1;i<=z;i++)
 {product*=x;
 }
 return product;
 }
-int length(int t)
+int length(int t)        //returns the length of the number
 {
 int flag=0;
 int i=0;
@@ -35,13 +36,13 @@ scanf("%d",&bin);
 size=length(bin);
 int temp1 ,temp2;
 temp1=bin;
-for(int i=(size-1);i>=0;i--)
+for(int i=(size-1);i>=0;i--)        
 {
-temp2=temp1/(power(i,10));
+temp2=temp1/(power(i,10));         //divides by highest degree term to return the binary digit of i'th term
 a[i]=temp2;
 if(temp2>=1)
-{temp1=temp1-power(i,10);
-num+=a[i]*power(i,2);
+{temp1=temp1-power(i,10);          // removes the highest digit binary 
+num+=a[i]*power(i,2);              //calculates the value of the decimal number
 if(temp1==0)
 {
 	i=-1;
